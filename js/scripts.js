ff18d21e71064166ab2a760a44a1716f14b1a8ae
ehuesca5 <ehuesca@vcd-ch204-24.eastern.ewu.edu>
2016-10-20 14:03:36 -0700

    v2

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,13 @@
 $('h1').lettering();
 
 // Intialize Backstretch
-$.backstretch('/nameplate/img/bg.jpg');
+$.backstretch('/img/bg.jpg');
 
 // Intialize Masonry
 $('article').masonry({
 	itemSelector: 'section',
 	columnWidth: 200,
 	gutter: 10,
+    isFitWidth: true
 	
 });
